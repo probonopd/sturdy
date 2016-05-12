a1bb0f405273964738cd46c5a153dbc98c30311e
Vladislav Tronko <innermous@gmail.com>
2016-05-12 21:17:02 +0300

    [Core] Add EntryManager

---
+++ src/core/entrymanager.cpp
@@ -0,0 +1,94 @@
+/***********************************************************************
+**
+** Sturdy - note-taking app
+** Copyright (C) 2016 Vladislav Tronko <innermous@gmail.com>
+**
+** Sturdy is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Sturdy. If not, see <http://www.gnu.org/licenses/>.
+**
+***********************************************************************/
+
+#include "entrymanager.h"
+
+#include <QDateTime>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QVariant>
+
+using namespace Core;
+
+EntryManager::~EntryManager()
+{
+    clear();
+}
+
+void EntryManager::clear()
+{
+    for (auto entryId : m_entries) {
+        save(entryId.first);
+        delete entryId.second;
+    }
+
+    m_entries.clear();
+}
+
+bool EntryManager::save(int entryId) const
+{
+    Entry* entry = m_entries.at(entryId);
+    if (!entry->isStateChanged)
+        return false;
+
+    QSqlDatabase db = QSqlDatabase::database();
+    if (db.isOpen())
+        return false;
+
+    entry->timestamp = QDateTime().toTime_t();
+
+    QSqlQuery query(db);
+    query.prepare(QStringLiteral("UPDATE entries SET content = ?, timestamp = ? WHERE id= ?"));
+    query.addBindValue(entry->content);
+    query.addBindValue(entry->timestamp);
+    query.addBindValue(entryId);
+    return query.exec();
+}
+
+bool EntryManager::load(int entryId) const
+{
+    QSqlDatabase db = QSqlDatabase::database();
+    if (db.isOpen())
+        return false;
+
+    QSqlQuery query(db);
+    query.prepare(QStringLiteral("SELECT id, content, timestamp FROM entries WHERE id= ?"));
+    query.addBindValue(entryId);
+    if (!query.exec())
+        return false;
+
+    query.next();
+
+    Entry* entry = new Entry;
+    entry->id = query.value(0).toInt();
+    entry->content = query.value(1).toString();
+    entry->timestamp = query.value(2).toUInt();
+
+    return true;
+}
+
+Entry* EntryManager::getEntry(int entryId) const
+{
+    auto it = m_entries.find(entryId);
+    if (it == m_entries.end())
+        load(entryId);
+
+    return m_entries.at(entryId);
+}
---
+++ src/core/entrymanager.h
@@ -0,0 +1,59 @@
+/***********************************************************************
+**
+** Sturdy - note-taking app
+** Copyright (C) 2016 Vladislav Tronko <innermous@gmail.com>
+**
+** Sturdy is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Sturdy. If not, see <http://www.gnu.org/licenses/>.
+**
+***********************************************************************/
+
+#ifndef ENTRYMANAGER_H
+#define ENTRYMANAGER_H
+
+#include <unordered_map>
+
+#include <QString>
+
+namespace Core
+{
+
+struct Entry
+{
+    int id;
+    QString content;
+    uint timestamp;
+
+    bool isStateChanged { false };
+};
+
+class EntryManager
+{
+public:
+    EntryManager() = default;
+    ~EntryManager();
+
+    Entry* getEntry(int entryId) const;
+
+    void clear();
+
+    bool save(int entryId) const;
+    bool load(int entryId) const;
+
+private:
+    std::unordered_map<int, Entry*> m_entries;
+};
+
+}
+
+#endif // ENTRYMANAGER_H
