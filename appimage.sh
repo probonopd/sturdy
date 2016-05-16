146e9df98b111c3c6247e777efe626b6d9463da3
probonopd <probonopd@users.noreply.github.com>
2016-05-16 22:15:20 +0200

    [ci skip]

--- appimage.sh
+++ appimage.sh
@@ -100,7 +100,7 @@ cat $LOWERAPP.desktop
 ########################################################################
 
 GLIBC_NEEDED=$(glibc_needed)
-VERSION=git$GIT_REV-glibc$GLIBC_NEEDED
+VERSION=git.$GIT_REV-glibc$GLIBC_NEEDED
 
 ########################################################################
 # Patch away absolute paths; it would be nice if they were relative
